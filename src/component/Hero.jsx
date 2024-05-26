492164e2f391325b1dffbe2b09d2774ee9d6de5f
Your Name <you@example.com>
2024-05-26 16:51:06 +0700

    update Hero

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -27,6 +27,8 @@ const Hero = () => {
               <TypeAnimation
                 sequence={[
                   // Same substring at the start will only be typed out once, initially
+                  "Frontend Developer",
+                  2000, 
                   "Full Stack Developer",
                   2000, // wait 1s before replacing "Mice" with "Hamsters"
                   "Software Developer",
